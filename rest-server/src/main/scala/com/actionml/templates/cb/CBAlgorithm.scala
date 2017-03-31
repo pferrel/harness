1dbf24298f851fb5fad96698118819dac77ff007
pferrel <pat@occamsmachete.com>
2017-03-31 09:47:30 -0700

    switched to develop version of CBAlgoritm with Semen's Actor system

--- rest-server/src/main/scala/com/actionml/templates/cb/CBAlgorithm.scala
+++ rest-server/src/main/scala/com/actionml/templates/cb/CBAlgorithm.scala
@@ -18,125 +18,125 @@
 package com.actionml.templates.cb
 
 import akka.actor._
-import akka.dispatch.{BoundedPriorityMailbox, PriorityGenerator}
+import akka.event.Logging
 import com.actionml.core.storage.Mongo
-import com.actionml.core.template.{AlgorithmParams, Algorithm}
+import com.actionml.core.template.{Algorithm, AlgorithmParams}
 import com.mongodb.casbah.MongoCollection
-import com.typesafe.config.Config
+import org.slf4j.event.SubstituteLoggingEvent
 
 import scala.concurrent.Future
-import scala.concurrent.duration.Duration
 
 
 /** Creates Actors for each group and does input event triggered training continually. The GroupTrain Actors
   * manager their own model persistence in true Kappa "micro-batch" style. Precessing typically small groups
-  * of events when a new one is detected, then updating the model for that group for subsequent queries. The GroupTrain
-  * Actors are managed by the CBAlgorithm and will be added and killed when needed.
+  * of events when a new one is detected, then updating the model for that group for subsequent queries.
+  * The GroupTrain Actors are managed by the CBAlgorithm and will be added and killed when needed.
   *
-  * @param p All needed params for the CB lib as well as the dataset containing events and data used in training. The
-  *          dataset will contain groups from which the GroupTrain Actors are created
+  * @param p All needed params for the CB lib as well as the dataset containing events and data used in training.
+  *          The dataset will contain groups from which the GroupTrain Actors are created
   */
-class CBAlgorithm(p: CBAlgoParams) extends Algorithm( new Mongo, p: CBAlgoParams ) {
+class CBAlgorithm(p: CBAlgoParams) extends Algorithm(new Mongo, p: CBAlgoParams) {
+
+  private val system = ActorSystem("CBAlgorithm")
+
+  private var trainers = Map.empty[String, ActorRef]
 
   // from the Dataset determine which groups are defined and start training on them
 
-  import context.dispatcher
-  val ticker = context.system.scheduler.schedule(0 millis, 5 second, self, Start)
+  def init(): CBAlgorithm = {
+    val groups: Map[String, MongoCollection] = p.dataset.CBCollections.usageEventGroups
+    logger.trace(s"Init manager for ${groups.size} groups. ${groups.mkString(", ")}")
+    val exists = trainers.keys.toList
+    val diff = groups.filterNot { case (key, _) ⇒
+      exists.contains(key)
+    }
 
-  implicit val system = ActorSystem(p.dataset.resourceId+"AKKA-SEED") // Todo: Semen is a unique id per template instance required?
+    // todo: Semen, not sure of the purpose of this
+    logger.trace("Existing trainers: {}", exists)
+    logger.trace("New trainers: {}", groups)
+    logger.trace("Diff trainers: {}", diff)
 
-  var trainers = p.dataset.CBCollections.usageEventGroups
-    .map { case(groupName, mongoCollection) =>
-      val trainer = context watch context.actorOf(Props[SingleGroupTrainer].withMailbox("custom-mailbox"), groupName)
-      groupName -> trainer
+    diff.foreach { case (trainer, collection) ⇒
+      val actor = system.actorOf(SingleGroupTrainer.props(collection), trainer)
+      trainers += trainer → actor
+    }
+    this
   }
 
   def train(groupName: String): Unit = {
-    trainers(groupName) !
+    logger.trace("Train trainer {}", groupName)
+    trainers(groupName) ! SingleGroupTrainer.Train
   }
 
   def remove(groupName: String): Unit = {
-    trainers = trainers - groupName
+    logger.info("Stop trainer {}", groupName)
+    system stop trainers(groupName)
+    logger.info("Remove trainer {}", groupName)
+    trainers -= groupName
   }
 
-  def add(groupName: String, mongoCollection: MongoCollection): Unit = {
-    trainers = trainers + ( groupName -> new SingleGroupTrainer(mongoCollection) )
+  def add(groupName: String, collection: MongoCollection): Unit = {
+    logger.info("Create trainer {}", groupName)
+    if (!trainers.contains(groupName)) {
+      val actor = system.actorOf(SingleGroupTrainer.props(collection), groupName)
+      trainers += groupName → actor
+    }
+  }
+
+  def stop(): Future[Terminated] = {
+    system.terminate()
   }
 
 }
 
 case class CBAlgoParams(
-    dataset: CBDataset, // required, where to get data
-    maxIter: Int = 100, // the rest of these are VW params
-    regParam: Double = 0.0,
-    stepSize: Double = 0.1,
-    bitPrecision: Int = 24,
-    modelName: String = "model.vw",
-    namespace: String = "n",
-    maxClasses: Int = 3)
+  dataset: CBDataset, // required, where to get data
+  maxIter: Int = 100, // the rest of these are VW params
+  regParam: Double = 0.0,
+  stepSize: Double = 0.1,
+  bitPrecision: Int = 24,
+  modelName: String = "model.vw",
+  namespace: String = "n",
+  maxClasses: Int = 3)
   extends AlgorithmParams
 
 
 class SingleGroupTrainer(events: MongoCollection) extends ActorWithLogging {
 
   import SingleGroupTrainer._
-  import context.dispatcher
 
-  override def receive: Receive = await
-
-  def await: Receive = {
-    case Run ⇒
-      log.info(s"${name} Receive 'Run', run job")
-      log.info(s"${name} Switch context to work")
-      context become work
-      startWork() recover {
-        case ex: Throwable ⇒ log.error(ex, "ERROR!")
-      } map { _ ⇒
-        log.info(s"${name} Work complete!")
-        log.info(s"${name} Switch context to await")
-        context become await
-      }
-
+  override def receive: Receive = {
+    case Train ⇒
+      log.info(s"$name Receive 'Train', run group training")
+      startWork()
   }
 
-  def work: Receive = {
-    case Run ⇒
-      log.info(s"${name} Received 'Run', not react, because already started job")
-  }
-
-  private def startWork(): Future[Unit] = {
-    log.info(s"${name} Start work")
-    Future {
-      Thread.sleep(15000)
-    }
+  private def startWork(): Unit = {
+    log.info(s"$name Start work")
+    log.info(s"$name Finish work")
   }
 
 }
 
 object SingleGroupTrainer{
-  case object Run
+
+  case object Train
+
+  def props(events: MongoCollection): Props = Props(new SingleGroupTrainer(events))
 }
-
-
 
 trait ActorWithLogging extends Actor with ActorLogging{
 
-  val name = self.path.name
+  protected val name: String = self.path.name
 
   override def preStart(): Unit = {
     super.preStart()
-    log.info(s"${Console.GREEN}Start actor ${name}${Console.RESET}")
+    log.info(s"${Console.GREEN}Start actor $name${Console.RESET}")
   }
 
   override def postStop(): Unit = {
     super.postStop()
-    log.info(s"${Console.RED}Stop actor ${name}${Console.RESET}")
+    log.info(s"${Console.RED}Stop actor $name${Console.RESET}")
   }
 
 }
-
-class CustomMailbox(settings: ActorSystem.Settings, config: Config) extends BoundedPriorityMailbox(
-  PriorityGenerator {
-    case SingleGroupTrainer.Run => 100
-    case PoisonPill => 1
-  }, capacity = 1, pushTimeOut = Duration("0 millis"))
